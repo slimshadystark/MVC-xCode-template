60d13be3856aaa957085e70ebeb8a9c3caa32c4f
slimshadystark <33152004+slimshadystark@users.noreply.github.com>
2020-01-14 13:24:08 +0100

    Add files via upload

---
+++ Model View Controller.xctemplate/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Cosimo Orlacchio on 13/01/2020.
+//  Copyright © 2020 Cosimo Orlacchio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Model View Controller.xctemplate/README_general.swift
@@ -0,0 +1,19 @@
+/*
+ Include inheritance and all protocol conformances at the beginning of the file, where the class is defined
+ In this way it's wasy to have a quick and clear idea of what the class does without scrolling down
+ Divide each file with MARKs to separate different sections in a logical and easy to understand way
+ 
+ e.g. See how to structure a ViewController in ./Controller/README_controller.swift
+ 
+ */
+
+/*
+ Comment each property or class with a brief description of what its purpose
+ 
+ Comment each method with the following structure, according to Swift Markup:
+ 
+    Description
+    A section for each Parameter
+    A Returns section for non-void methods
+ 
+ */
